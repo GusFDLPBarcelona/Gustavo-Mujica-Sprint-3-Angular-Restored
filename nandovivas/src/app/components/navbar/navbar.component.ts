7bb9d102ae1c795307a4bafaca5b77037ef560fb
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2025-03-27 12:14:22 +0100

    Completa actualización de Contact component, wellcome component y otras actualizaciones. La web entra en fase de definición para desplegar wellcome, contact.

--- nandovivas/src/app/components/navbar/navbar.component.ts
+++ nandovivas/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
-  templateUrl: './navbar.component.html', // Ruta correcta
+  imports: [RouterModule, MatBottomSheetModule],
+  templateUrl: './navbar.component.html', 
   styleUrls: ['./navbar.component.css'],
   standalone: true,
 })
 export class NavbarComponent {
   isMenuVisible = false;
+
+  constructor(private bottomSheet: MatBottomSheet) {}
 
   toggleMenu() {
     console.log('isMenuVisible:', this.isMenuVisible); 
@@ -19,5 +24,12 @@ export class NavbarComponent {
   navigateAndCloseMenu() {
     this.isMenuVisible = false;
   }
+
+  openContactSheet(): void {
+    this.bottomSheet.open(ContactComponent);
+  }
+
+
+
 }
 
