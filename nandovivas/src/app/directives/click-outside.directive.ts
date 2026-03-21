cc38d702b7aac8ed7f3756c59ee5660b4e2f3a36
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2026-03-21 22:02:40 +0100

    fix(directive): corregir tipo EventTarget en ClickOutsideDirective

--- nandovivas/src/app/directives/click-outside.directive.ts
+++ nandovivas/src/app/directives/click-outside.directive.ts
@@ -16,7 +16,8 @@ import {
     constructor(private elementRef: ElementRef) {}
   
     @HostListener('document:click', ['$event.target'])
-    public onClick(targetElement: HTMLElement) {
+    public onClick(targetElement: EventTarget | null) {
+      if (!(targetElement instanceof HTMLElement)) return;
       const clickedInside = this.elementRef.nativeElement.contains(targetElement);
       if (!clickedInside) {
         this.clickOutside.emit();
